74402845e179e7889b5b2b17c741db5fdd45df7c
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-06-22 23:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let submitBtn = document.getElementById('submit');
+let checkBox = document.getElementById('checkbox');
+
+var data = localStorage.getItem("myKey");
+if (data !== null) {
+	let btn = document.createElement('button');
+}
+
+submitBtn.addEventListener('click',(event)=>{
+	event.preventDefault();
+	if (username.value === "" || password.value === "") {
+		return;
+	}
+	let user = {
+		name:username,
+		pass:password,
+	}
+	if (checkBox.checked) {
+		localStorage.setItem('test',JSON.stringify(user));
+	}
+	alert(`Logged in as ${username.value}`);
+});
+		
+
+
+
